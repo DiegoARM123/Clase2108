e2d6906cf7b69f138842bcc56f89f1f16892d194
adrian <adrian1084@gmail.com>
2018-08-23 11:59:50 -0500

    primeros exercise de JS

---
+++ JS/Funciones.js
@@ -0,0 +1,19 @@
+alert("Benvenidos")
+
+function myFunction2(){
+    document.getElementById("demo1").innerHTML="Parrafo Cambia";
+ }
+
+function myFunction1(){
+    document.getElementById("demo2").innerHTML="Parrafo Cambia";
+}
+
+function myFunction3() {
+    document.getElementById("demo3").innerHTML="Paragrafo cambiante";
+}
+
+function myFunction4()
+ {
+    document.getElementById("demo4").style.fontSize='40px';
+
+}
